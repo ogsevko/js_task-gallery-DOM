cd2a96eb2c15e6c93463422cc2a4c3bc8c101c63
Yaroslav Andriyenko <andriyenko.yaroslav@gmail.com>
2020-06-17 16:46:42 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-console.log('hello mate academy!');
+function gallery(element) {
+  const imageList = element.querySelector('#thumbs');
+
+  imageList.addEventListener('click', (clickEvent) => {
+    const selected = clickEvent.target.closest('.gallery__img');
+    const largeImage = element.querySelector('#largeImg');
+
+    if (selected || imageList.contains(selected)) {
+      clickEvent.preventDefault();
+
+      const imageSrc = selected
+        .getAttribute('src')
+        .replace('-thumb.jpeg', '.png');
+
+      largeImage.setAttribute('src', imageSrc);
+    }
+  });
+}
+
+gallery(document.querySelector('.gallery'));
